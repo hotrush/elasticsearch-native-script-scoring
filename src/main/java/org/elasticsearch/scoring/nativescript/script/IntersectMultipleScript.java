1342403b8a48bcf15642d4ed52743e28d67652fb
rush <johny64@gmail.com>
2014-07-15 12:19:13 +0300

    added release 1.0.4

--- src/main/java/org/elasticsearch/scoring/nativescript/script/IntersectMultipleScript.java
+++ src/main/java/org/elasticsearch/scoring/nativescript/script/IntersectMultipleScript.java
@@ -1,6 +1,7 @@
 package org.elasticsearch.scoring.nativescript.script;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -8,7 +9,6 @@ import org.elasticsearch.script.ScriptException;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.index.fielddata.ScriptDocValues;
-import org.elasticsearch.index.fielddata.ScriptDocValues.Longs;
 import org.elasticsearch.script.AbstractSearchScript;
 import org.elasticsearch.script.ExecutableScript;
 import org.elasticsearch.script.NativeScriptFactory;
@@ -50,14 +50,22 @@ public class IntersectMultipleScript extends AbstractSearchScript {
 
     @Override
     public Object run() {
-        source_items = (ArrayList<String>) doc().get(field);
-        source_items.retainAll(items);
-        int intersections_num = source_items.size();
-        if (intersections_num > 0) {
-            if (intersections_num >= limit) {
-                return score() + limit*multiple;
-            } else {
-                return score() + intersections_num*multiple;
+        //source_items = (ArrayList<String>) doc().get(field);
+        ScriptDocValues source_doc_values = (ScriptDocValues) doc().get(field);
+        if (source_doc_values != null && !source_doc_values.isEmpty()) {
+
+            List<String> source_items_list = ((ScriptDocValues.Strings) source_doc_values).getValues();
+            ArrayList<String> source_items = new ArrayList<String>(source_items_list.size());
+            source_items.addAll(source_items_list);
+
+            source_items.retainAll(items);
+            int intersections_num = source_items.size();
+            if (intersections_num > 0) {
+                if (intersections_num >= limit) {
+                    return score() + limit*multiple;
+                } else {
+                    return score() + intersections_num*multiple;
+                }
             }
         }
         return score();
