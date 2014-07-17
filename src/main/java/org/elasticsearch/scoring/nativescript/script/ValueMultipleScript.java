deb0fbebf59ee993a31d780093f13081aebc573c
rush <johny64@gmail.com>
2014-07-17 15:22:14 +0300

    added release 1.0.5 / ValueMiltipleScoring fixes

--- src/main/java/org/elasticsearch/scoring/nativescript/script/ValueMultipleScript.java
+++ src/main/java/org/elasticsearch/scoring/nativescript/script/ValueMultipleScript.java
@@ -55,8 +55,8 @@ public class ValueMultipleScript extends AbstractSearchScript {
             ScriptDocValues.Longs fieldValue = (ScriptDocValues.Longs) source_doc_value;
 
             for (HashMap<String, Double> value_multiple : value_multiples) {
-                //if (value_multiple.get("value") == fieldValue.getValue()) {
-                if (value_multiple.get("value") == 2) {
+                if (value_multiple.get("value") == fieldValue.getValue()) {
+                //if (value_multiple.get("value") == 2) {
                     //return score()*value_multiple.get("multiple");
                     return score()*1000;
                 }
