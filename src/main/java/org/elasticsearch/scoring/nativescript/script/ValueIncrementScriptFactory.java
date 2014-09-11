d73b664890c5a6c5cff77a5285494edd1e2d8ecd
hotrush <johny64@gmail.com>
2014-09-11 12:13:32 +0300

    added release 1.2.1

--- src/main/java/org/elasticsearch/scoring/nativescript/script/ValueIncrementScriptFactory.java
+++ src/main/java/org/elasticsearch/scoring/nativescript/script/ValueIncrementScriptFactory.java
@@ -37,14 +37,15 @@ public class ValueIncrementScriptFactory implements NativeScriptFactory {
         @Override
         public float runAsFloat() {
             ScriptDocValues source_doc_value = (ScriptDocValues) doc().get(field);
-            //if (source_doc_value != null && !source_doc_value.isEmpty()) {
+            if (source_doc_value != null && !source_doc_value.isEmpty()) {
 
                 ScriptDocValues.Longs fieldValue = (ScriptDocValues.Longs) source_doc_value;
 
-                return fieldValue.getValue();
+                //return fieldValue.getValue();
+                return 100;
 
-            //}
-            //return 0;
+            }
+            return 0;
         }
     }
 }
