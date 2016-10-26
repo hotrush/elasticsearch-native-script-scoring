b7ac414532eb65160b143a5208e70c8f653856d3
rush <johny64@gmail.com>
2016-10-26 16:20:55 +0300

    upgrade to elasticsearch 2.4 version

---
+++ src/main/java/org/elasticsearch/plugin/scoring/IntersectMultipleStringScriptFactory.java
@@ -0,0 +1,70 @@
+package org.elasticsearch.plugin.scoring;
+
+import java.util.*;
+
+import org.elasticsearch.script.ScriptException;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
+import org.elasticsearch.index.fielddata.ScriptDocValues;
+//import org.elasticsearch.script.AbstractFloatSearchScript;
+import org.elasticsearch.script.AbstractFloatSearchScript;
+import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.NativeScriptFactory;
+
+public class IntersectMultipleStringScriptFactory implements NativeScriptFactory {
+
+    @Override
+    public ExecutableScript newScript(@Nullable Map<String, Object> params) {
+        int limit = params == null ? null : XContentMapValues.nodeIntegerValue(params.get("limit"), 0);
+        int multiple = params == null ? null : XContentMapValues.nodeIntegerValue(params.get("multiple"), 0);
+        String field = params == null ? null : XContentMapValues.nodeStringValue(params.get("field"), null);
+        String itemsStr = params == null ? null : XContentMapValues.nodeStringValue(params.get("items"), null);
+        ArrayList<String> items = new ArrayList<String>(Arrays.asList(itemsStr.split(",,,")));
+        if (field == null || items == null) {
+            throw new ScriptException("Missing the field parameter");
+        }
+        return new IntersectMultipleStringScript(field,items,limit,multiple);
+    }
+
+    @Override
+    public boolean needsScores() {
+      return false;
+    }
+
+    private static class IntersectMultipleStringScript extends AbstractFloatSearchScript {
+
+        private final String field;
+        private final int limit;
+        private final int multiple;
+        private final ArrayList<String> items;
+
+        public IntersectMultipleStringScript(String field,ArrayList<String> items,int limit, int multiple) {
+            this.field = field;
+            this.limit = limit;
+            this.multiple = multiple;
+            this.items = items;
+        }
+
+        @Override
+        public float runAsFloat() {
+            ScriptDocValues source_doc_values = (ScriptDocValues) doc().get(field);
+            if (source_doc_values != null && !source_doc_values.isEmpty()) {
+
+                List<String> source_items_list = ((ScriptDocValues.Strings) source_doc_values).getValues();
+                ArrayList<String> source_items = new ArrayList<String>(source_items_list.size());
+                source_items.addAll(source_items_list);
+
+                source_items.retainAll(items);
+                int intersections_num = source_items.size();
+                if (intersections_num > 0) {
+                    if (intersections_num >= limit) {
+                        return (float) limit*multiple;
+                    } else {
+                        return (float) intersections_num*multiple;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/org/elasticsearch/plugin/scoring/ValueInRangeScriptFactory.java
@@ -0,0 +1,64 @@
+package org.elasticsearch.plugin.scoring;
+
+import java.util.*;
+
+import org.elasticsearch.script.ScriptException;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
+import org.elasticsearch.index.fielddata.ScriptDocValues;
+import org.elasticsearch.index.fielddata.ScriptDocValues.Longs;
+import org.elasticsearch.script.AbstractFloatSearchScript;
+import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.NativeScriptFactory;
+
+public class ValueInRangeScriptFactory implements NativeScriptFactory {
+
+    @Override
+    public ExecutableScript newScript(@Nullable Map<String, Object> params) {
+        int multiple = params == null ? null : XContentMapValues.nodeIntegerValue(params.get("multiple"), 0);
+        String range_field1 = params == null ? null : XContentMapValues.nodeStringValue(params.get("range_field1"), null);
+        String range_field2 = params == null ? null : XContentMapValues.nodeStringValue(params.get("range_field2"), null);
+        long value = params == null ? null : XContentMapValues.nodeLongValue(params.get("value"), 0);
+        return new ValueInRangeScript(multiple,range_field1,range_field2,value);
+    }
+
+    @Override
+    public boolean needsScores() {
+      return false;
+    }
+
+    private static class ValueInRangeScript extends AbstractFloatSearchScript {
+
+        private final int multiple;
+        private final String range_field1;
+        private final String range_field2;
+        private final long value;
+
+        public ValueInRangeScript(int multiple,String range_field1,String range_field2,long value) {
+            this.multiple = multiple;
+            this.range_field1 = range_field1;
+            this.range_field2 = range_field2;
+            this.value = value;
+        }
+
+        @Override
+        public float runAsFloat() {
+            ScriptDocValues sourceRangeDoc1 = (ScriptDocValues) doc().get(range_field1);
+            ScriptDocValues sourceRangeDoc2 = (ScriptDocValues) doc().get(range_field2);
+
+            if (sourceRangeDoc1 != null && !sourceRangeDoc1.isEmpty() && sourceRangeDoc2 != null && !sourceRangeDoc2.isEmpty()) {
+
+                ScriptDocValues.Longs fieldRangeValue1 = (ScriptDocValues.Longs) sourceRangeDoc1;
+                ScriptDocValues.Longs fieldRangeValue2 = (ScriptDocValues.Longs) sourceRangeDoc2;
+
+                if (value >= fieldRangeValue1.getValue() && value <= fieldRangeValue2.getValue()) {
+                    return (float) multiple*1;
+                }
+
+            }
+
+            return 0;
+
+        }
+    }
+}
